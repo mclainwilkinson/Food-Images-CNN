ebe0c5d262601cc979fd2974f61d68b851f6c782
mclainwilkinson <mclain.wilkinson@gmail.com>
2018-11-28 15:35:43 -0500

    4-layer network

--- food_CNN.py
+++ food_CNN.py
@@ -9,8 +9,8 @@ import time
 
 # -----------------------------------------------------------------------------------
 # training parameters
-batch_size = 30
-num_epochs = 1
+batch_size = 40
+num_epochs = 50
 
 # train and test set files
 h5train_file = "food_train.h5"
@@ -38,27 +38,39 @@ test_dataset = DatasetFromHdf5(h5test_file)
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 # -----------------------------------------------------------------------------------
 # define the network model
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 40, kernel_size=8),
+        self.layer1 = nn.Sequential(               # input size = 64 x 64
+            nn.Conv2d(3, 100, kernel_size=8, padding=4),
+            nn.BatchNorm2d(100),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2, padding=1))  # output size = 33 x 33
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(100, 60, kernel_size=8, padding=4),
+            nn.BatchNorm2d(60),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2))             # output size = 17 x 17
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(60, 40, kernel_size=8, padding=2),
             nn.BatchNorm2d(40),
             nn.ReLU(),
-            nn.MaxPool2d(2, stride=2))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(40, 15, kernel_size=4),
-            nn.BatchNorm2d(15),
+            nn.MaxPool2d(2, stride=2))             # output size = 7 x 7
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(40, 20, kernel_size=4, padding=1),
+            nn.BatchNorm2d(20),
             nn.ReLU(),
-            nn.MaxPool2d(2, stride=2, padding=1))
-        self.fc = nn.Linear(13 * 13 * 15, 15)
+            nn.MaxPool2d(2, stride=2))             # output size = 3 x 3
+        self.fc = nn.Linear(3 * 3 * 20, 15)
 
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
         out = out.view(out.size(0), -1)
         out = self.fc(out)
         return out
@@ -75,6 +87,7 @@ optimizer = torch.optim.Adam(cnn.parameters(), lr=0.001)
 # Train the Model
 num_images = train_dataset.__len__()
 start = time.time()
+print('Training...')
 
 for epoch in range(num_epochs):
     for i, (data, target) in enumerate(train_loader):
@@ -116,14 +129,21 @@ for images, labels in test_loader:
             correct_array[actual] += 1
 # -----------------------------------------------------------------------------------
 # display overall results
-print('Test Accuracy of the model on the test images: %d %%' % (100 * correct / total))
+print('Accuracy of the model on the test images: %d %%' % (100 * correct / total))
 print('')
 
 # display individual category results
 print('Individual class accuracy:')
-target_classes = (target_class.decode() for target_class in train_dataset.classes)
+target_classes = tuple(target_class.decode() for target_class in train_dataset.classes)
 for i, t in enumerate(target_classes):
     print(t, '%.2f' % (100 * correct_array[i] / total_array[i]), '%')
+
+print('correct', correct_array)
+print('total', total_array)
+pct_array = correct_array / total_array
+best = int(np.argmax(pct_array))
+print('class with highest accuracy:', target_classes[best],
+      '%.2f' % (100 * correct_array[best] / total_array[best]), '%')
 # -----------------------------------------------------------------------------------
 # Save the Trained Model
 torch.save(cnn.state_dict(), 'cnn.pkl')
